d44e3b540fab56de40fc6887aa49b798abb7d370
hokin <hokin.think@qq.com>
2022-12-17 23:36:14 +0800

    17

---
+++ day/2022_12/17/my_strncmp.c
@@ -0,0 +1,26 @@
+// int strncmp(const char* str1, const char* str2, size_t count);
+#include <stdio.h>
+
+int my_strncmp(const char* str1,const char* str2,int count)
+{
+  while(count--)
+  {
+    if(*str1 == *str2)
+    {
+      str1++;
+      str2++;
+    }
+    else
+    {
+      return (int)(*str1-*str2);
+    }
+  }
+}
+
+int main()
+{
+  const char* str1 = "hello";
+  const char* str2 = "world"; 
+  printf("%d\n",my_strncmp(str1,str2,3));
+  return 0;
+}
