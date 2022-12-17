91b7674caa2e7a363e3784bbde6af2782bbf6d85
hokin <hokin.think@qq.com>
2022-12-17 23:51:54 +0800

    17

--- day/2022_12/17/my_strncmp.c
+++ day/2022_12/17/my_strncmp.c
@@ -1,8 +1,10 @@
 // int strncmp(const char* str1, const char* str2, size_t count);
 #include <stdio.h>
+#include <assert.h>
 
 int my_strncmp(const char* str1,const char* str2,int count)
 {
+  assert(str1 && str2);
   while(count--)
   {
     if(*str1 == *str2)
